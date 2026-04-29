606fbe2e62972e92fe79edc0f4b9200d8ac748c1
twinklessingh11-design <twinkles.singh11@gmail.com>
2026-04-29 16:21:58 +0530

    final syntax fix

--- server/server.js
+++ server/server.js
@@ -21,6 +21,9 @@ app.use(clerkMiddleware())
 //Api routes
 app.get('/',(req,res)=>res.send('Server is live!'))
 // app.use('/api/inngest',serve({client:inngest,functions}))
-app.use('/api/inngest',serve({client:inngest,functions:functions,signingKey:signkey-prod-f4fd8bfad39f781d80363ee59bdd2a3ea13a88250e437cacf00059d9ae9720d1}));
-
+app.use('/api/inngest', serve({ 
+  client: inngest, 
+  functions: functions,
+  signingKey: 'signkey-prod-f4fd8bfad39f781d80363ee59bdd2a3ea13a88250e437cacf00059d9ae972' 
+}));
 app.listen(port,()=>console.log(`Server listening at http://localhost:${port}`));
